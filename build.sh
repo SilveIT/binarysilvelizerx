ae7cdfcc3f62b7c076a3cdc48120355879d311e8
SilverIce <silverice.yuriy@gmail.com>
2018-10-09 15:16:43 +0300

    * Fixed pipeline build file for example project;

--- build.sh
+++ build.sh
@@ -16,6 +16,12 @@ cp Build/netstandard2.0/publish/BinarySilvelizerX.dll Build/
 cp Build/netstandard2.0/publish/BinarySilvelizerX.deps.json Build/
 cp Build/netstandard2.0/publish/BinarySilvelizerX.pdb Build/
 
+cp Build/BinarySilvelizer.Example/netcoreapp2.1/publish/BinarySilvelizerX.Example.dll Build/BinarySilvelizer.Example/
+cp Build/BinarySilvelizer.Example/netcoreapp2.1/publish/BinarySilvelizerX.Example.deps.json Build/BinarySilvelizer.Example/
+cp Build/BinarySilvelizer.Example/netcoreapp2.1/publish/BinarySilvelizerX.Example.runtimeconfig.json Build/BinarySilvelizer.Example/
+cp Build/BinarySilvelizer.Example/netcoreapp2.1/publish/BinarySilvelizerX.Example.pdb Build/BinarySilvelizer.Example/
+
 rm -rf "Build/netstandard2.0"
+rm -rf "Build/BinarySilvelizer.Example/netcoreapp2.1"
 
 echo "Success"
